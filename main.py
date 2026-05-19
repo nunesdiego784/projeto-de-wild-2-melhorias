f1f25790452a31d75047ec70bc8755cc5dd731d9
Gabriel <gabrielnunesfigueiredo324@gmail.com>
2026-05-19 13:54:12 -0300

    teste

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import customtkinter as ctk
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title("CustomTkinter App")
+        self.title("Projeto do corno do wild ")
         self.geometry("400x300")
 
-        self.label = ctk.CTkLabel(self, text="Hello, CustomTkinter!")
+        self.label = ctk.CTkLabel(self, text="va se fuder wild")
         self.label.pack(pady=20)
 
-        self.button = ctk.CTkButton(self, text="Click Me", command=self.button_click)
+        self.button = ctk.CTkButton(self, text="clique aqui ", command=self.button_click)
         self.button.pack(pady=10)
 
     def button_click(self):
-        self.label.configure(text="Button Clicked!")
+        self.label.configure(text="pau no cu de wild")
 if __name__ == "__main__":
     app = App()
     app.mainloop()
